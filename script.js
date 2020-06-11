164878083ee7bcf436444e3840ba896e8b7c9265
Aidan Starr <aidan.starr19@gmail.com>
2020-06-10 21:23:00 -0400

    What is happening

---
+++ script.js
@@ -0,0 +1,25 @@
+class Products {
+    constructor(name, category, description, price) {
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+}
+
+let prod1 = new Products('Cyberpunk 2077', ['PC', 'Xbox One', 'PS4'], 'The Witcher, but like in the future.', 59.99);
+let prod2 = new Products('Last Of Us: Part II', ['PS4'], 'An action survival game where zombies and humans can be bad', 59.99);
+let prod3 = new Products('Overwatch',['PC', 'Xbox One', 'PS4'], 'THE class based shooter', 19.99);
+let prod4 = new Products('Jet Set Radio Future',['Xbox'], 'An old game about roller skating and graffiti', 32.99);
+let prod5 = new Products('E.T', ['Atari'], 'The game was put in the landfill for a reason', 17.99);
+let prod6 = new Products('Resident Evil',['PC', 'Xbox One', 'PS4'], 'A classic remastered with new features and better graphics', 19.99);
+let prod7 = new Products('Starcraft', ['PC'], 'Screw your zerg rush', 9.99);
+let prod8 = new Products('Superman 64', ['Nintendo 64'], 'It\'s really bad and really expensive', 399.94);
+let prod9 = new Products('Valorant', ['PC'], 'Class based shooter in the style of CS:GO', 19.99);
+let prod10 = new Products('Give us money',['N/A'], 'There is no product, we just want your money. Feel free to buy this more than once. :)', 5);
+
+console.log(prod3)
+
+document.getElementById('').addEventListener('click', e=>{
+    
+})
