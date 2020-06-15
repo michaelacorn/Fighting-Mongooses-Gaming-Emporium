3fc0f71f60732ef64bc6a72687b6cfcee1bc7be8
Aidan Starr <aidan.starr19@gmail.com>
2020-06-15 19:10:05 -0400

    Merge time

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 class Products {
-    constructor(name, category, description, price) {
+    constructor(name, category, description, price, imgsrc) {
         this.name = name;
         this.category = category;
         this.description = description;
         this.price = price;
-        this.imgsrc = this.imgsrc;
+        this.imgsrc = imgsrc || '';
     }
 }
 let prodArr = [];
 
-prodArr.push(new Products('Cyberpunk 2077', ['PC', 'Xbox One', 'PS4'], 'The Witcher, but like in the future.', 59.99, '../images/Cyberpunk2077_XBone'));
-prodArr.push(new Products('Last Of Us: Part II', ['PS4'], 'An action survival game where zombies and humans can be bad', 59.99));
-prodArr.push(new Products('Overwatch',['PC', 'Xbox One', 'PS4'], 'THE class based shooter', 19.99));
-prodArr.push(new Products('Jet Set Radio Future',['Xbox'], 'An old game about roller skating and graffiti', 32.99));
-prodArr.push(new Products('E.T', ['Atari'], 'The game was put in the landfill for a reason', 17.99));
-prodArr.push(new Products('Resident Evil',['PC', 'Xbox One', 'PS4'], 'A classic remastered with new features and better graphics', 19.99));
-prodArr.push(new Products('Starcraft', ['PC'], 'Screw your zerg rush', 9.99));
-prodArr.push(new Products('Superman 64', ['Nintendo 64'], 'It\'s really bad and really expensive', 399.94));
-prodArr.push(new Products('Valorant', ['PC'], 'Class based shooter in the style of CS:GO', 19.99));
-prodArr.push(new Products('Give us money',['N/A'], 'There is no product, we just want your money. Feel free to buy this more than once. :)', 5));
+prodArr.push(new Products('Cyberpunk 2077', ['PC', 'Xbox One', 'PS4'], 'The Witcher, but like in the future.', 59.99, 'images/Cyberpunk2077_XBone.jpg'));
+prodArr.push(new Products('Last Of Us: Part II', ['PS4'], 'An action survival game where zombies and humans can be bad', 59.99, 'images/LOU2_PS4.jpg'));
+prodArr.push(new Products('Overwatch',['PC', 'Xbox One', 'PS4'], 'THE class based shooter', 19.99, 'images/OW_PC.jpg'));
+prodArr.push(new Products('Jet Set Radio Future',['Xbox'], 'An old game about roller skating and graffiti', 32.99, 'images/JSRF_Xbox.jpg'));
+prodArr.push(new Products('E.T', ['Atari'], 'The game was put in the landfill for a reason', 17.99, 'images/ET_2600.jpg'));
+prodArr.push(new Products('Resident Evil',['PC', 'Xbox One', 'PS4'], 'A classic remastered with new features and better graphics', 19.99,'images/RE_PS4.jpg'));
+prodArr.push(new Products('Starcraft', ['PC'], 'Screw your zerg rush', 9.99, 'images/Starcraft_PC.jpg'));
+prodArr.push(new Products('Superman 64', ['Nintendo 64'], 'It\'s really bad and really expensive', 399.94, 'images/Superman64.jpg'));
+prodArr.push(new Products('Valorant', ['PC'], 'Class based shooter in the style of CS:GO', 19.99, 'images/Valorant_PC.png'));
+prodArr.push(new Products('Give us money',['N/A'], 'There is no product, we just want your money. Feel free to buy this more than once. :)', 5, 'images/Gib_Money.png'));
 
 const cart = document.getElementById('cart')
 const shelf = document.getElementById('game-container')
